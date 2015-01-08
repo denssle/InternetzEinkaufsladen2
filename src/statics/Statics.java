b812ca3bc0a31749830b59468722007dbabffafb
Dominik Enßle <dominik.enssle@innoq.com>
2015-01-08 09:39:08 +0100

    Statics update: locations

--- src/statics/Statics.java
+++ src/statics/Statics.java
@@ -11,4 +11,10 @@ public class Statics
 		"Registration"
 	};
 	public static final String[] benutzerlabels = {"Name", "Email", "Geburtstag", "Strasse", "Hausnummer", "Stadt", "Postleitzahl", "Passwort"};
+	
+	public static final int loc_down = 180;
+	public static final int loc_up = 0;
+	public static final int loc_left = 0;
+	public static final int loc_right = 710;
+	
 }
