6227ba02be2979c1a9a5cce5dd2aa81681821faf
Dominik Enßle <dominik.enssle@innoq.com>
2015-01-08 10:17:37 +0100

    warenkorb Aktualisierung

--- src/controller/WarenkorbController.java
+++ src/controller/WarenkorbController.java
@@ -1,5 +1,21 @@
 package controller;
 
-public class WarenkorbController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import view.WarenkorbView;
+
+public class WarenkorbController implements ActionListener
+{
+	private WarenkorbView warenkorbV = new WarenkorbView(this);
+	
+	public void warenkorbAnzeigen()
+	{
+		warenkorbV.show();
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		
+	}
 }
--- src/view/WarenkorbView.java
+++ src/view/WarenkorbView.java
@@ -1,8 +1,17 @@
 package view;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 
 public class WarenkorbView extends JFrame
 {
-
+	public WarenkorbView(ActionListener warenkorbController)
+	{
+	}
+	
+	public void show()
+	{
+		this.setVisible(true);
+	}
 }
