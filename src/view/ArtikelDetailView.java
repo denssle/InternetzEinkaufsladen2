c0fb6b9b8864bbbd9333e252b7d4d2934d9fc2b6
Dominik Enßle <dominik.enssle@innoq.com>
2015-01-09 10:15:42 +0100

    ArtikelDetailUpdate

--- src/view/ArtikelDetailView.java
+++ src/view/ArtikelDetailView.java
@@ -1,10 +1,25 @@
 package view;
 
-public class ArtikelDetailView
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+import statics.Statics;
+import model.ArtikelModel;
+
+public class ArtikelDetailView extends JFrame
 {
-	
-	public void anzeigen()
+	public ArtikelDetailView()
 	{
-		
+		this.setLayout(new FlowLayout());		
+		this.setSize(700,700);
+		this.setDefaultCloseOperation(javax.swing.JFrame.HIDE_ON_CLOSE);
+		this.setLocation(Statics.loc_left, Statics.loc_down);
+	}
+	
+	public void anzeigen(ArtikelModel anzuzeigenderArtikel)
+	{
+		this.setVisible(true);
+		this.setTitle(anzuzeigenderArtikel.getName());
 	}
 }
