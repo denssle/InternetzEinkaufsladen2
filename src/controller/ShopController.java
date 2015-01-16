de0b55ea3a089332b754fd1d082e378d4f8ba520
Dominik Enßle <dominik.enssle@innoq.com>
2015-01-16 13:29:52 +0100

    better prices

--- src/controller/ShopController.java
+++ src/controller/ShopController.java
@@ -35,7 +35,7 @@ public class ShopController implements ActionListener
 			neuerArtikel.setName("Testartikel Nr.: "+(i-1));
 			neuerArtikel.setBeschreibung("Testbeschreibung: "+diceRoller.toString());
 			neuerArtikel.setKategorie("Testkategorie: " + diceRoller.nextBoolean());
-			neuerArtikel.setPreis(diceRoller.nextInt());
+			neuerArtikel.setPreis(diceRoller.nextDouble()+i);
 			dao.speichern(neuerArtikel);
 		}
 	}
